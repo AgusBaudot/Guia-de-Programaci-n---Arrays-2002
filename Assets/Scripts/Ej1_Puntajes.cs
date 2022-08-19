b312630d311fcdda102cf71c756e3868cf6ac9b0
AgusBaudot <mamuilmalal2005@gmail.com>
2022-08-19 10:44:52 -0300

    Ej1 Done

---
+++ Assets/Scripts/Ej1_Puntajes.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1_Puntajes : MonoBehaviour
+{
+    [SerializeField] int[] puntajes = new int[10];
+    //ObtenerNivelDeMenorPuntaje()
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(ObtenerMayorPuntaje());
+        Debug.Log(ObtenerNivelDeMenorPuntaje());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int ObtenerMayorPuntaje()
+    {
+        int mayor = 0;
+        foreach (int n in puntajes)
+        {
+            if (n > mayor)
+            {
+                mayor = n;
+            }
+        }
+        return mayor;
+    }
+
+    int ObtenerNivelDeMenorPuntaje()
+    {
+        int nivel = 1;
+        int menor = puntajes[0];
+        for (int i = 0; i < puntajes.Length; i++)
+        {
+            if (puntajes[i] < menor)
+            {
+                menor = puntajes[i];
+                nivel = i;
+            }
+        }
+        return nivel;
+    }
+
+    float ObtenerPuntajePromedio()
+    {
+        float total = 0;
+        float promedio;
+
+        for (int i = 0; i < puntajes.Length; i++)
+        {
+            total += puntajes[i];
+        }
+
+        promedio = total / puntajes.Length;
+
+        return promedio;
+    }
+
+}
---
+++ Assets/Scripts/asd.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asd : MonoBehaviour
+{
+    [SerializeField] string[] meses = new string[12] {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    [SerializeField] int[] dias = new int[12] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    [SerializeField] string FechaIngresada;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ValidarFormatoFecha(FechaIngresada);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool ValidarFormatoFecha(string fecha)
+    {
+        bool fechaValida = false;
+        string dia;
+        string mes;
+        if(fecha.Length == 4)
+        {
+            dia = "";
+            dia += fecha[0];
+            dia += fecha[1];
+            int diaNumber = int.Parse(dia);
+            mes = "";
+            mes += fecha[2];
+            mes += fecha[3];
+            int.Parse(mes);
+            int mesNumber = int.Parse(mes);
+            if(diaNumber>0 && diaNumber <= 31 && mesNumber > 0 && mesNumber <= 12)
+            {
+                fechaValida = true;
+            }
+        }
+        else
+        {
+            Debug.Log("El largo de la fecha es incorrecto");
+        }
+        return fechaValida;
+    }
+}
